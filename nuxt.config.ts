abcab69de92d48f7f976da601b4b2521e3aed24d
Jesse Bakels <jesse.bakels@jumbo.com>
2024-03-13 14:22:07 +0100

    fix: fixes for studio

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/ui"],
+  modules: ["@nuxt/ui", "@nuxthq/studio"],
   extends: ["./libs/content-layer"],
   colorMode: {
     preference: "dark",
